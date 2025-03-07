322ad8bf7013957ebca4955159e31cfe9b286de1
Mingze Li <15610159165lmz@gmail.com>
2025-03-07 09:03:09 +0800

    建立状态机和状态接口的文件

---
+++ GameState.h
@@ -0,0 +1,8 @@
+#pragma once
+
+/**
+ * @file GameState.h
+ * @author 李明泽
+ * @brief 定义了游戏的不同状态所具备的共有接口，供状态机使用。
+ * @version 1.0
+ */
---
+++ StateMachine.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/**
+ * @file StateMachine.h
+ * @author 李明泽
+ * @brief 整体流程控制采取FSM有限状态机模式。
+ *			该文件是状态机，用于控制游戏流程，在不同的界面、状态之间切换。
+ * @version 1.0
+ */
