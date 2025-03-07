ac07d8885a5a6b9638fb2044d10411c05ea2168c
Mingze Li <15610159165lmz@gmail.com>
2025-03-07 09:22:08 +0800

    初步构建两个类，部分函数

--- GameState.h
+++ GameState.h
@@ -6,3 +6,7 @@
  * @brief 定义了游戏的不同状态所具备的共有接口，供状态机使用。
  * @version 1.0
  */
+
+class GameState {
+
+};
--- StateMachine.h
+++ StateMachine.h
@@ -7,3 +7,24 @@
  *			该文件是状态机，用于控制游戏流程，在不同的界面、状态之间切换。
  * @version 1.0
  */
+
+#include <string>
+#include <map>
+#include "GameState.h"
+
+class StateMachine{
+public:
+	std::map<std::string, GameState> gameStates;
+
+	StateMachine() {
+
+	}
+
+
+	void update(int x, int y);
+
+	void render();
+
+	void changeState(std::string to);
+
+}
