427204adcfa7de22d81fcee84672e23357a100ac
Mingze Li <15610159165lmz@gmail.com>
2025-03-07 14:15:00 +0800

    增加StartMenu，是一种GameState

---
+++ StartMenu.h
@@ -0,0 +1,18 @@
+#pragma once
+
+/**
+ * @file StartMenu.h
+ * @author 李明泽
+ * @brief 游戏开始时的菜单界面
+ * @version 1.0
+ */
+
+#include "StateMachine.h"
+#include "GameState.h"
+
+
+class StartMenu : public GameState {
+public:
+
+
+};
