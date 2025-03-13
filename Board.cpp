029399a817d7ff4c4b9f68585b4946a5a328c3f8
Mingze Li <15610159165lmz@gmail.com>
2025-03-13 21:04:04 +0800

    处理棋盘源文件

--- Board.cpp
+++ Board.cpp
@@ -8,6 +8,66 @@
 #include "Board.h"
 
 
+std::unique_ptr<Board> Board::create() {
+
+}
+
+
+void Board::addChess(std::unique_ptr<Chess> chess) {
+
+}
+
+
+Chess* Board::chessAt(ExMessage& msg) {
+    return NULL;
+}
+
+
+bool Board::isMoveChess(Chess* chess, int newX, int newY) {
+    return false;
+}
+
+
+void Board::drawTriangle(int p_x, int p_y, const char c, int n, COLORREF color) {
+    
+}
+
+
+void Board::draw() {
+
+}
+
+
+void Board::hashIndex() {
+
+}
+
+
+void Board::buildadj() {
+
+}
+
+
+int Board::handleClick(ExMessage& msg) {
+    return 0;
+}
+
+
+void Board::findSingleJumpMove(Chess* chess) {
+
+}
+
+
+void Board::findDoybleJumpMove(Chess* chess, int step) {
+
+}
+
+
+void Board::getJumpMove(Chess* chess) {
+
+}
+
+
 Chess* Board::isHereAChess(ExMessage& msg) {
 	return NULL;
 }
@@ -18,6 +78,7 @@ bool Board::moveChess(Chess* chess) {
 }
 
 
-bool Board::isChessWin(std::string color) {
-	return false;
+bool Board::isChessWin(COLORREF color) {
+    return false;
 }
+
