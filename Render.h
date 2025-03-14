aee434a9f1ad0f9ede4c0f05516c71bb7eaae8a1
Mingze Li <15610159165lmz@gmail.com>
2025-03-14 13:43:51 +0800

    调整绘图部分的所有函数

--- Render.h
+++ Render.h
@@ -20,6 +20,19 @@
 
 
  /**
+  * @brief 绘制棋子
+  *
+  * 详细描述函数的功能、参数、返回值等信息。
+  *
+  * @param 参数1 描述参数1的作用
+  * @param 参数2 描述参数2的作用
+  * @return 返回值的描述
+  * @author 作者名字
+  */
+void drawChess();
+
+
+ /**
   * @brief 绘制六边形网格
   *
   * 详细描述函数的功能、参数、返回值等信息。
@@ -31,27 +44,28 @@
 void drawHexagonGrid();
 
 
-/**
- * @brief 绘制棋子
+/*
+    负责人:
+    功能: 绘制三角形，
+    同时使用addChess添加棋子，
+    state 记录顶点的状态  例如空设置0
+          根据不同的颜色设置不同的状态
+    pos记录所有棋子的坐标
+
+
+
+    参数: p_x,p_y 为起始坐标   n 表示画几层  c 表示三角形是正的还是倒的
+    color颜色
+    返回值：无返回值
+*/
+void drawTriangle(int p_x, int p_y, const char c, int n, COLORREF color);
+
+
+/*
+ * @brief 绘制开始菜单
  *
  * 详细描述函数的功能、参数、返回值等信息。
  *
- * @param 参数1 描述参数1的作用
- * @param 参数2 描述参数2的作用
- * @return 返回值的描述
- * @author 作者名字
- */
-void drawChess();
-
-
-/**
- * @brief 
- *
- * 详细描述函数的功能、参数、返回值等信息。
- *
- * @param 参数1 描述参数1的作用
- * @param 参数2 描述参数2的作用
- * @return 返回值的描述
  * @author 作者名字
  */
 void drawStartMenu();
@@ -83,11 +97,12 @@ void drawPlayerNum();
 void drawPlayerInfo();
 
 
-
    /**
 	* @brief 
 	*
-	* 详细描述函数的功能、参数、返回值等信息。
+	* 	绘制棋盘和棋子,可是使用4个4层的三角形组成外围，
+	* 上下由9层的正三角，8层的倒三角组成，坐标已给出
+	* 调用6个drawTriangle 函数
 	*
 	* @param 参数1 描述参数1的作用
 	* @param 参数2 描述参数2的作用
