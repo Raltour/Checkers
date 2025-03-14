68c0c9b4c75716716b4ef133269ef3468c8eb9b9
Mingze Li <15610159165lmz@gmail.com>
2025-03-14 13:09:13 +0800

    微调

--- Button.h
+++ Button.h
@@ -1,15 +1,18 @@
 #pragma once
 
 /**
- * @file MyClass.cpp
- * @brief 该文件实现了 MyClass 类的功能。
- * @author
+ * @file Button.h
+ * @brief 用户通过鼠标与按钮交互，实现流程控制
+ * 
+ * 利用EasyX的消息功能为按钮实现相应交互的能力
+ * 所有的实现直接写在这个头文件中，不需要另开源文件
+ * 
+ * @author 杜云飞
  * @version 2.3
  */
 
 
 #include <easyx.h>
-#include <graphics.h>
 #include <string>
 
 class Button
@@ -22,22 +25,41 @@ class Button
     COLORREF m_oldColor;
     COLORREF m_changeColor;  //改变颜色
     COLORREF m_currentColor; //当前颜色
+
+
 public:
-    Button(int x, int y, int width, int height, const std::string& text);
+    Button(int x, int y, int width, int height, const std::string& text) {
+
+    }
+
 
     //绘制按钮
-    void draw() const;
+    void drawButton() const {
+
+    }
+
 
     //按钮点击（检查是否在按钮中）
-    bool isClicked(int mouseX, int mouseY)const;
+    bool isClicked(int mouseX, int mouseY)const {
+
+    }
+
 
     //设置文本文件
-    void setText(const std::string& text);
+    void setText(const std::string& text) {
+
+    }
+
 
     //设置颜色
-    void setColor(COLORREF changeColor);
+    void setColor(COLORREF changeColor) {
+
+    }
+
 
     //恢复原色，默认颜色
-    void removeColor();
+    void removeColor() {
+
+    }
 
 };
